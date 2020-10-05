097c8518dd1c1ac6b07c142488418136533e6d96
Martin Hedlund <martinhedlund@live.com>
2020-10-05 10:35:06 +0200

    1.5 disable

--- js/views/sidebarPresentation.jsx
+++ js/views/sidebarPresentation.jsx
@@ -1,7 +1,9 @@
 const SidebarPresentation = ({ guests, setGuests }) => {
   return (
     <div>
-      <button onClick={() => setGuests(guests - 1)}>-</button>
+      <button onClick={() => setGuests(guests - 1)} disabled={guests < 2}>
+        -
+      </button>
       <button onClick={() => setGuests(guests + 1)}>+</button>
     </div>
   );
